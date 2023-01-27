9f10b3554ef34c0cfc517229655b7690d7f6e014
Jack Lögdahl <110182175+Jacklog8@users.noreply.github.com>
2023-01-27 21:05:32 +0100

    Fixes so that the Sunbeam countdown doesn't overlap with the blueprint tracker.
    Yet another request from a member in the Subnautica modding discord.

---
+++ SunbeamCountdownFix_SN/BepInEx.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using System.Reflection;
+using System.IO;
+
+namespace SunbeamCountdownFix_SN
+{
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    public class BepInEx : BaseUnityPlugin
+    {
+        private const string myGUID = "com.JackLog.Sunbeamtimer";
+        private const string pluginName = "Sunbeam Timer SN";
+        private const string versionString = "1.0.0";
+
+        private static readonly Harmony harmony = new Harmony(myGUID);
+
+        public static ManualLogSource logger;
+
+        private void Awake()
+        {
+            harmony.PatchAll();
+            Logger.LogInfo(pluginName + " " + versionString + " " + "loaded.");
+            logger = Logger;
+        }
+    }
+}
---
+++ SunbeamCountdownFix_SN/TimerFix.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace SunbeamCountdownFix_SN
+{
+    internal class TimerFix
+    {
+        [HarmonyPatch(typeof(uGUI_SunbeamCountdown))]
+        public class uGUI_SunbeamCountdown_Patch
+        {
+            [HarmonyPatch(nameof(uGUI_SunbeamCountdown.Start))]
+            [HarmonyPostfix]
+            public static void uGUI_SunbeamCountdown_Start_Patch(uGUI_SunbeamCountdown __instance)
+            {
+                __instance.gameObject.transform.localPosition = new Vector3(-1496.492f, 0, 0);
+            }
+        }
+    }
+}
