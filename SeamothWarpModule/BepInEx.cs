85756addf027c128e915ecef7c1c51a3d1d42828
Jack Lögdahl <110182175+Jacklog8@users.noreply.github.com>
2023-02-09 21:22:20 +0100

    Added Seamoth Warp Module mod

---
+++ SeamothWarpModule/BepInEx.cs
@@ -0,0 +1,76 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using SMLHelper.V2.Options.Attributes;
+using System.Reflection;
+using System.IO;
+using SMLHelper.V2.Json;
+using SMLHelper.V2.Handlers;
+using System.Collections;
+
+namespace SeamothWarpModule_SN
+{
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    public class BepInEx : BaseUnityPlugin
+    {
+        public static AssetBundle assetBundle = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "warpassets"));
+
+        private const string myGUID = "com.JackLog.Warp.Module";
+        private const string pluginName = "Seamoth Warp Module SN";
+        private const string versionString = "1.0.0";
+
+        private static readonly Harmony harmony = new Harmony(myGUID);
+
+        public static ManualLogSource logger;
+
+        public static float Distance = 30;
+
+        private void Awake()
+        {
+            harmony.PatchAll();
+            Logger.LogInfo(pluginName + " " + versionString + " " + "loaded.");
+            logger = Logger;
+
+            SeamothModuleItem item = new SeamothModuleItem();
+            item.Patch();
+        }
+
+        public static FMODAsset GetFmodAsset(string audioPath)
+        {
+            FMODAsset asset = ScriptableObject.CreateInstance<FMODAsset>();
+            asset.path = audioPath;
+            return asset;
+        }
+
+        public static IEnumerator GetHoloMaterial(IOut<Material> @out)
+        {
+            UWE.IPrefabRequest task = UWE.PrefabDatabase.GetPrefabAsync("4e372190-a687-49bf-a89a-d134c949546e");
+            yield return task;
+            task.TryGetPrefab(out GameObject pf);
+            GameObject go;
+            go = Instantiate<GameObject>(pf, Vector3.zero, Quaternion.identity, true);
+
+            yield return new WaitForSecondsRealtime(5);
+            MeshRenderer[] renderers = go.FindChild("hologram").FindChild("worlddisplay").FindChild("wireframeworld").FindChild("MiniWorld").GetAllComponentsInChildren<MeshRenderer>();
+
+            @out.Set(renderers[0].material);
+        }
+
+        /*public static IEnumerator GetMaterialFromClassIDAsync(string classId, IOut<Material> material, int rendererIndex = 0)
+        {
+            material = null;
+
+            UWE.IPrefabRequest task = UWE.PrefabDatabase.GetPrefabAsync(classId);
+            yield return task;
+            task.TryGetPrefab(out GameObject forceFieldPrefab);
+
+            MeshRenderer[] meshRenderers = forceFieldPrefab.GetAllComponentsInChildren<MeshRenderer>();
+
+            if (meshRenderers != null)
+                Debug.LogWarning("Loacted mesh renderers.");
+
+            material.Set(meshRenderers[rendererIndex].material);
+        } */
+    }
+}
---
+++ SeamothWarpModule/SeamothModuleItem.cs
@@ -0,0 +1,61 @@
+﻿using SMLHelper.V2.Assets;
+using UnityEngine;
+using SMLHelper.V2.Crafting;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace SeamothWarpModule_SN
+{
+    internal class SeamothModuleItem : Equipable
+    {
+        public static TechType ModuleTechtype;
+
+        public SeamothModuleItem() : base("SeamothWarp", "Warp module", "Reuses Warper technology to allow the user to warp forward every once in a while.") 
+        {
+            OnFinishedPatching += () =>
+            {
+                ModuleTechtype = TechType;
+            };
+        }
+
+        public override TechCategory CategoryForPDA => TechCategory.VehicleUpgrades;
+        public override CraftTree.Type FabricatorType => CraftTree.Type.SeamothUpgrades;
+        public override TechGroup GroupForPDA => TechGroup.VehicleUpgrades;
+        public override TechType RequiredForUnlock => TechType.PrecursorLostRiverWarperParts;
+        public override Vector2int SizeInInventory => new Vector2int(1, 1);
+        public override QuickSlotType QuickSlotType => QuickSlotType.Selectable;
+        public override float CraftingTime => 3;
+        public override string[] StepsToFabricatorTab => new string[] { "SeamothModules" };
+        public override EquipmentType EquipmentType => EquipmentType.SeamothModule;
+
+        protected override TechData GetBlueprintRecipe()
+        {
+            return new TechData()
+            {
+                craftAmount = 1,
+
+                Ingredients = new List<Ingredient>(new Ingredient[]
+                    {
+                        new Ingredient(TechType.PrecursorIonCrystal, 3),
+                        new Ingredient(TechType.TitaniumIngot, 1),
+                        new Ingredient(TechType.PrecursorKey_Purple, 1),
+                        new Ingredient(TechType.Kyanite, 2)
+                    }
+                )
+            };
+        }
+
+        protected override Atlas.Sprite GetItemSprite()
+        {
+            Atlas.Sprite sprite = new Atlas.Sprite(BepInEx.assetBundle.LoadAsset<Sprite>("Icon"), true);
+
+            return sprite;
+
+        }
+        public override IEnumerator GetGameObjectAsync(IOut<GameObject> gameObject)
+        {
+            yield return CraftData.InstantiateFromPrefabAsync(TechType.VehicleHullModule1, gameObject);
+        }
+    }
+}
